3c7707b40315da4fa67d590ca54f5cc2ecd5bed2
Dmitry <kdp.029029@gmail.com>
2020-05-04 22:47:15 +0300

    First home task on the course

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
-let incr = 10,
-    decr = 10;
+let money,
+    time,
+    firstAnswer,
+    secondAnswer,
+    thirdAnswer,
+    fourthAnswer;
 
-console.log(incr++);
-console.log(decr--);
-console.log(5 % 2);
+let appdata = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: {},
+    savings: false
+};   
 
-console.log("2" == 2);
-console.log("2" === 2);
+money = +prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
+firstAnswer = prompt("Введите обязательную статью расходов в этом месяце");
+secondAnswer = +prompt("Во сколько обойдется?");
+thirdAnswer = prompt("Введите обязательную статью расходов в этом месяце");
+fourthAnswer = +prompt("Во сколько обойдется?");
+
+appdata.budget = money;
+appdata.timeData = time;
+appdata.expenses.firstAnswer = secondAnswer;
+appdata.expenses.thirdAnswer = fourthAnswer;
+
+let dayBudget = (appdata.budget - appdata.expenses.firstAnswer - appdata.expenses.thirdAnswer)/30;
+
+alert("Бюджет на 1 день " + dayBudget);
